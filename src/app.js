0293bcc96b6f7ab695e9b7f2f30e15a25d62d4a2
Tania Olivia <tania.olivia69@yahoo.com>
2023-09-04 15:22:17 +0200

    ajoute fichier .env et l'utilise dans la connexion de bd

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 const mongoose = require("mongoose");
  
 mongoose.set('strictQuery', false);
 
 // Connexion à la base de données MongoDB
-mongoose.connect("mongodb://mongo/backdb", {
+mongoose.connect(`mongodb://${process.env.ME_CONFIG_MONGODB_SERVER}/${process.env.ME_CONFIG_MONGODB_AUTH_DATABASE}`, {
     useNewUrlParser: true,
-    user: "dockerback",
-    pass: "backpass"
+    user: process.env.ME_CONFIG_MONGODB_AUTH_USERNAME,
+    pass: process.env.ME_CONFIG_MONGODB_AUTH_PASSWORD
 }).then(() => {
     console.log('Connexion à la base de données avec succès');
 }).catch(err => {
