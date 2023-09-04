d7412a39b5db81e807f714080a1a966cb5e23485
Tania Olivia <tania.olivia69@yahoo.com>
2023-09-04 14:15:55 +0200

    ajoute le fichier app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const mongoose = require("mongoose");
+ 
+mongoose.set('strictQuery', false);
+
+// Connexion à la base de données MongoDB
+mongoose.connect("mongodb://mongo/backdb", {
+    useNewUrlParser: true,
+    user: "dockerback",
+    pass: "backpass"
+}).then(() => {
+    console.log('Connexion à la base de données avec succès');
+}).catch(err => {
+    console.log('Erreur de connexion à la base de données');
+    process.exit();
+});
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+
 // Définition de la route "Hello, World!"
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -9,5 +28,5 @@ app.get('/', (req, res) => {
 
 // Écoute du serveur sur le port spécifié
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Serveur est lancé au port ${port}`);
 });
